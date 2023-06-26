bfb9279f1e620529768a03491faad4654a78466c
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-26 18:18:15 +0530

    Change password dto

---
+++ backend/src/main/java/dev/abhisek/backend/dto/ChangePassword.java
@@ -0,0 +1,16 @@
+package dev.abhisek.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ChangePassword {
+    private String oldPassword;
+    private String newPassword;
+    private String re_Password;
+}
