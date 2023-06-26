edb1071755f716580efef1b75ff538ba773a9dee
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-26 18:16:23 +0530

    Login or Register request dto

---
+++ backend/src/main/java/dev/abhisek/backend/dto/AuthRequest.java
@@ -0,0 +1,15 @@
+package dev.abhisek.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AuthRequest {
+    private String email;
+    private String password;
+}
