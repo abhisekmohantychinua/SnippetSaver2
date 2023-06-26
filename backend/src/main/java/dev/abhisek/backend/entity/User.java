1f1c12ccb56f89092b016512846bcdd6bed4aa16
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-26 18:26:11 +0530

    User entity

---
+++ backend/src/main/java/dev/abhisek/backend/entity/User.java
@@ -0,0 +1,59 @@
+package dev.abhisek.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Document(collection = "users")
+public class User implements UserDetails {
+    @MongoId
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Roles roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(roles.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
