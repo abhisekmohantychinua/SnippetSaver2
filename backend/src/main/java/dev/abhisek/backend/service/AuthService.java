e358ef57592fe9c78df88a5e5c019cb658e8061e
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-07-31 09:24:50 +0530

    service interface for authentication

--- backend/src/main/java/dev/abhisek/backend/service/AuthService.java
+++ backend/src/main/java/dev/abhisek/backend/service/AuthService.java
@@ -1,17 +1,18 @@
 package dev.abhisek.backend.service;
 
-import dev.abhisek.backend.dto.AuthRequest;
-import dev.abhisek.backend.dto.ChangePassword;
-import dev.abhisek.backend.dto.UserDto;
-import dev.abhisek.backend.dto.UserRequestDto;
+import dev.abhisek.backend.dto.auth.AuthRequest;
+import dev.abhisek.backend.dto.auth.AuthResponse;
+import dev.abhisek.backend.dto.auth.ChangePassword;
+import dev.abhisek.backend.dto.user.UserResponseDto;
+import dev.abhisek.backend.dto.user.UserRequestDto;
 import dev.abhisek.backend.entity.RegisterRequest;
 
 public interface AuthService {
     void addRegisterRequest(RegisterRequest request);
 
-    UserDto verifyRegisterToken(String token, UserRequestDto userRequestDto);
+    UserResponseDto verifyRegisterToken(String token, UserRequestDto userRequestDto);
 
-    String authorize(AuthRequest request);
+    AuthResponse authorize(AuthRequest request);
 
     void forgetPasswordRequest(String email);
 
